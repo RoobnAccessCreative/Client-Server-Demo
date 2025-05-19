795617b6859b85c3f4037dac4fbcc67e67d31c34
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-05-19 16:08:06 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.listen(8080, () => {
+  console.log("Welcome to port 8080");
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "You've found me.. >:(" });
+});
+
+app.post("/sending", (req, res) => {
+  const userSent = req.body;
+  res.send(`That's a wonderful ${userSent} you've got there..`);
+});
