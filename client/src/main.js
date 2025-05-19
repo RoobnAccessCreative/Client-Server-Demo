b6a18adaf1ffcf4a5f36a23deaff59725de88904
RoobnAccessCreative <reuben.dubois.000@accesscreative.ac.uk>
2025-05-19 16:43:17 +0100

    added fetch request

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +19,13 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
+
+const baseUrl = "https://client-server-demo.onrender.com/";
+
+(async () => {
+  const request = await fetch(baseUrl);
+  request ? console.log("ladies and gentlemen, we got em") : null;
+})();
